13a880a853a5e7734f792d9262572b4b25f1058a
Pepijn Frenken <454799@student.fontys.nl>
2021-11-10 10:32:26 +0100

    update

---
+++ WebSockies/HelloWorldModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WebSockies
+{
+    public class HelloWorldModel: MessageModel
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ WebSockies/MessageModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebSockies
+{
+    public class MessageModel
+    {
+        public string Controller { get; set; }
+        public string Method { get; set; }
+        public string Parameters { get; set; }
+    }
+}

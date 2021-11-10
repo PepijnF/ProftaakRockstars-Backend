0fb4b02eb42bb6bf6026b92722949490b4b71999
Ebe Koudijds <ebekoudijs@gmail.com>
2021-11-10 10:48:55 +0100

    Auto stash before rebase of "origin/master"
    Code Changes

---
+++ WebSockies/Data/Lobby.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proftaak.Data
+{
+    public class Lobby
+    {
+        public string Id { get; set; }
+        public string OwnerId { get; set; }
+        public string InviteCode { get; set; }
+        public List<User> Users { get; set; }
+        public bool IsOpen { get; set; }
+
+        public Lobby(string userId, string userName)
+        {
+            Id = Guid.NewGuid().ToString();
+            OwnerId = userId;
+
+            Random rand = new Random();
+            InviteCode = rand.Next(1000,9999).ToString();
+            
+            Users = new List<User>();
+            Users.Add(new User(){Id = userId, Name = userName, Score = 0});
+
+            IsOpen = true;
+        }
+    }
+}
